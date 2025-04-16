ef47ad9f142eccbf02cf1ded6751fdea35efb29f
babak <babakus7@gmail.com>
2025-04-16 14:22:34 +0330

    font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@vueuse/nuxt', 'shadcn-nuxt', '@nuxt/fonts'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
   css: ['~/assets/css/main.css'],
   app: {
     head: {
@@ -13,6 +14,12 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100;200;300;400;500;600;700;800;900&display=swap'
+        }
       ]
     }
   }
